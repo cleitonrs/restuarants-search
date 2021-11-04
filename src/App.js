e0f2f133095510c401ceee9deabd25713dde3e6f
cleitonrs <rscleitonrs@gmail.com>
2021-11-04 19:48:01 -0300

    create home page and reseted styles

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
-
+import Home from "./pages/Home";
+import { ThemeProvider } from "styled-components";
+import { Reset } from "styled-reset";
+import theme from "./theme"
 
 function App() {
   return (
-    <div className="App">
-      <h1>Hello World</h1>
-    </div>
+    <ThemeProvider theme={theme}>
+      <Reset />
+      <Home />
+    </ThemeProvider>
   );
 }
 
