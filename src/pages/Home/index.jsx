86f859137fbc9a46577edcd7e9a7f8e28c2765d9
cleitonrs <rscleitonrs@gmail.com>
2021-11-05 14:36:57 -0300

    input added

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,9 +1,27 @@
-import React from "react";
-import { Container } from "./styles";
+import React, { useState } from "react";
+import logo from "../../assets/logo.svg"
+import { Container, Search } from "./styles";
+import TextField, { Input } from '@material/react-text-field';
+
 
 const Home = () => {
+  const [inputValue, setInputValue] = useState('')
+
   return (
-    <Container>Hello World!</Container>
+    <Container>
+      <Search>
+        <img src={logo} alt="Logo do restaurante" />
+        <TextField
+          label='Pesquisar'
+          outlined
+        //onTrailingIconSelect={() => this.setState({ value: '' })}
+        //trailingIcon={<MaterialIcon role="button" icon="delete" />}
+        ><Input
+            value={inputValue}
+            onChange={(e) => setInputValue(e.target.value)} />
+        </TextField>
+      </Search>
+    </Container>
   )
 }
 
--- src/pages/Home/styles.js
+++ src/pages/Home/styles.js
@@ -6,4 +6,10 @@ export const Container = styled.aside`
   height: 100vh;
   overflow-y: auto;
 `
-
+export const Search = styled.section`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  background-color: #ffffff;
+  padding: 16px;
+`
