c957cfa11d0f440ce6e4a23e4cd9daca30b3d74d
cleitonrs <rscleitonrs@gmail.com>
2021-11-06 17:15:17 -0300

    component part2

--- src/components/RetaurantCard/index.jsx
+++ src/components/RetaurantCard/index.jsx
@@ -1,9 +1,15 @@
 import React from "react";
-import { Restaurant } from "./styles";
+import { Restaurant, RestaurantInfo, Title, Address } from "./styles";
 
 const RestaurantCard = () => {
   return (
-    <Restaurant />
+    <Restaurant>
+      <RestaurantInfo>
+        <Title>Nome do Restaurante</Title>
+        <p>Avaliação</p>
+        <Address>Rua Rio de Janeiro, 120</Address>
+      </RestaurantInfo>
+    </Restaurant>
   )
 }
 
