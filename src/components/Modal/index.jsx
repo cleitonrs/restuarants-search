33eea50cfdf9bdcd8813c7dac4c55aedef099caf
cleitonrs <rscleitonrs@gmail.com>
2021-11-08 07:02:58 -0300

    component part4

---
+++ src/components/Modal/index.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Portal from "./Portal"
+
+const Modal = () => {
+  return (
+    <Portal>
+      <div>fdafadf</div>
+    </Portal>
+  )
+}
+
+export default Modal
